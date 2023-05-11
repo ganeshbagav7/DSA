9826626fd0defddcd712acf3607dfcf1fb6b387c
ganeshbagav <ganeshbagav2001@gmail.com>
2023-05-12 00:09:05 +0530

    Added Create Tree Using Post-order and In-order

--- Tree/createBinaryTree.c
+++ Tree/createBinaryTree.c
@@ -7,6 +7,7 @@ typedef struct TreeNode
     int data;
     struct TreeNode *left;
     struct TreeNode *right;
+    //createBinar yTree
 } tnode;
 
 tnode *head = NULL;
@@ -42,7 +43,16 @@ void print_in_order(tnode *root)
     printf(" %d ", root->data);
     print_in_order(root->right);
 }
-
+void print_post_order(tnode *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    print_post_order(root->left);
+    print_post_order(root->right);
+    printf(" %d ", root->data);
+}
 int findIndex(int data , int arr[] , int size ){
     for (int i = 0; i < size; i++)
     {
@@ -74,6 +84,24 @@ tnode * createTreeUsingInAndPreOrderArray( int inOrderArray[] , int preOrderArra
 
 }
 
+tnode * createTreeUsingInAndPostOrderArray( int inOrderArray[] , int preOrderArray[] , int start , int end  , int size){
+    
+    if(start > end ){
+        return NULL;
+    }
+    static int nodeNo=1;
+    int index = findIndex( preOrderArray[size-nodeNo] , inOrderArray , size );
+    nodeNo++;
+
+    tnode * newNode = createNode(inOrderArray[index]);
+    
+    newNode->right = createTreeUsingInAndPostOrderArray(inOrderArray ,preOrderArray ,index+1  , end , size);
+    newNode->left = createTreeUsingInAndPostOrderArray(inOrderArray, preOrderArray , start , index-1 , size);
+
+    return newNode;
+
+}
+
 void main()
 {
     int size;
@@ -83,6 +111,7 @@ void main()
 
     int inOrderArray[size];
     int preOrderArray[size];
+    int postOrderArray[size];
 
     printf(" Enter Data in In-order array : \n");
     for (int i = 0; i < size; i++)
@@ -97,10 +126,24 @@ void main()
         scanf("%d", &preOrderArray[i]);
     }
 
+        printf(" Enter Data in post-order array : \n");
+    for (int i = 0; i < size; i++)
+    {
+        printf(" Enter Data : ");
+        scanf("%d", &postOrderArray[i]);
+    }
+
     head = createTreeUsingInAndPreOrderArray(inOrderArray ,preOrderArray , 0 , size-1 , size);
     printf("\n In-order : \n");
     print_in_order(head);
     printf("\n Pre-order : \n");
     print_pre_order(head);
+
+    head = createTreeUsingInAndPostOrderArray(inOrderArray ,postOrderArray , 0 , size-1 , size);
+
+    printf("\n\nIn-order : \n");
+    print_in_order(head);
+    printf("\n Post-order : \n");
+    print_post_order(head);
 
 }
